c6cf863a71e0c1ddf717f3c0439eb565f45d9b8b
Ambrish <ambrish19.5211@gmail.com>
2023-12-14 00:21:57 +0530

    Added about Us Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Footer from "./Components/Footer";
+import AboutUs from "./Pages/AboutUs";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} ></Route>
+      <Route path="/about" element={<AboutUs />} ></Route>
     </Routes>
   );
 }

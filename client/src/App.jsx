aae5038e72f8f63c7a0c52928afdb5719d9f061d
Ambrish <ambrish19.5211@gmail.com>
2023-12-13 19:04:45 +0530

    Added home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
 import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} ></Route>
+    </Routes>
   );
 }
 

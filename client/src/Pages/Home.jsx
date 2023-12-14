b8693b1832aa920b5f516ec0c431e0ffc6aedab3
Ambrish <ambrish19.5211@gmail.com>
2023-12-14 21:22:18 +0530

    addded typewriter in home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,39 +1,50 @@
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
-import homePageMainImage from '../assets/Images/homePageMainImage.png';
+import homePageMainImage from "../assets/Images/homePageMainImage.png";
 import HomeLayout from "../Layouts/HomeLayout";
 function Home() {
+  return (
+    <HomeLayout>
+      <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
+        <div className="w-1/2 space-y-6">
+          <h1 className="text-5xl font-semibold">
+            Find out best{" "}
+            <span className="text-yellow-500 font-bold">
+              <Typewriter
+                options={{
+                  strings: ["Affordable", "Online Courses"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+          </h1>
+          <p className="text-xl text-gray-200">
+            We have a large library of courses taught by highly skilled and
+            qualified faculties at a very affordable cost.
+          </p>
 
-    return (
-        <HomeLayout>
-            <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
-                <div className="w-1/2 space-y-6">
-                    <h1 className="text-5xl font-semibold">Find out best <span className="text-yellow-500 font-bold">Online Courses</span></h1>
-                    <p className="text-xl text-gray-200">
-                        We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
-                    </p>
+          <div className="space-x-6">
+            <Link to="/courses">
+              <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                Explore courses
+              </button>
+            </Link>
+            <Link to="/contacts">
+              <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                Contact Us
+              </button>
+            </Link>
+          </div>
+        </div>
 
-                    <div className="space-x-6">
-                        <Link to="/courses" >
-                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
-                                Explore courses
-                            </button>
-                        </Link>
-                        <Link to="/contacts" >
-                            <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
-                                Contact Us
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-                
-                <div className="w-1/2 flex items-center justify-center">
-                    <img src={homePageMainImage} alt="home page" />
-                </div>
-
-            </div>
-        </HomeLayout>
-    )
+        <div className="w-1/2 flex items-center justify-center">
+          <img src={homePageMainImage} alt="home page" />
+        </div>
+      </div>
+    </HomeLayout>
+  );
 }
 
 export default Home;

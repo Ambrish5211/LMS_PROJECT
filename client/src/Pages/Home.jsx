a6102a5e6d0b65efa7d83d697ce8789da20d7be2
Ambrish <ambrish19.5211@gmail.com>
2023-12-18 23:23:20 +0530

    Denied page implemented

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
                 Explore courses
               </button>
             </Link>
-            <Link to="/contacts">
+            <Link to="/contact">
               <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
                 Contact Us
               </button>

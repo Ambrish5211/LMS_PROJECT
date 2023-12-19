e2ecca3db524f82f539e06a4624b04a5b572074d
Ambrish <ambrish19.5211@gmail.com>
2023-12-19 23:24:11 +0530

    Added course description page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./Pages/AboutUs";
 import Contact from "./Pages/Contact";
+import CourseDescription from "./Pages/Course/CourseDescription";
 import CourseList from "./Pages/Course/CourseList";
 import Denied from "./Pages/Denied";
 import Home from "./Pages/Home";
@@ -19,6 +20,7 @@ function App() {
       <Route path="/signup" element={<Signup />} ></Route>
       <Route path="/signin" element={<Signin />} ></Route>
       <Route path="/courses" element={<CourseList />} ></Route>
+      <Route path="/course/description" element={<CourseDescription />} ></Route>
       <Route path="/contact" element={<Contact />} ></Route>
       <Route path="/denied" element={<Denied />} ></Route>
       <Route path="*" element={<NotFound />} ></Route>

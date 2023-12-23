c700b74fde7d177750e016801eca58e0523b0d73
Ambrish <ambrish19.5211@gmail.com>
2023-12-23 17:06:53 +0530

    remove .env file from git

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,21 +1,70 @@
+import AppError from "../utils/appError.js"
 
 
 export const getRazorpayApiKey = async (req, res, next) => {
+  try {
+     res.status(200).json({
+      success:true,
+      message: 'RAZORPAY_API_KEY:',
+      key: process.env.RAZORPAY_KEY_ID  
+     })
+  } catch (error) {
+    return next(
+      new AppError(error.message, 500)
+    )
+  }
 
 }
 
 export const buySubscription = async (req, res, next) => {
+  try {
+    const {id} = req.user;
+    const user = await User.findById(id);
 
+    if(!user){
+      return next(new AppError('Unauthorized, please login', 500))
+    }
+
+    if(user.role == "ADMIN"){
+      return next(new AppError('Admin cannot buy a subscription', 500))
+
+    }
+
+
+
+  } catch (error) {
+    return next(
+      new AppError(error.message, 500)
+    )
+  }
 }
 
-export const verifySubscription = () => {
-
+export const verifySubscription = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    return next(
+      new AppError(error.message, 500)
+    )
+  }
 }
 
-export const cancelSubscription = () => {
-
+export const cancelSubscription = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    return next(
+      new AppError(error.message, 500)
+    )
+  }
 }
 
-export const getAllPayments = () => {
-
+export const getAllPayments = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    return next(
+      new AppError(error.message, 500)
+    )
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 import cloudinary from 'cloudinary';
-
+import Razorpay from 'razorpay'
 
 const PORT = process.env.PORT || 5001;
 
@@ -9,6 +9,12 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
   api_key:process.env.CLOUDINARY_API_KEY
 })
+
+
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
 
 app.listen(PORT,() => {
   console.log(`Server is listening on port : ${PORT}`);

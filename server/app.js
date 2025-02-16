45f5564d673cdd69c34e6d8f7d2299b57ed2b431
Ambrish <ambrish19.5211@gmail.com>
2025-02-16 21:45:19 +0530

    build error fix 7

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ import morgan from 'morgan';
 
 const app = express();
 app.use(cors({
-   origin: ['http://localhost:5173'],
+   origin: ['http://localhost:5173', 'https://lms-project-roan-ten.vercel.app'],
    credentials:true
 }));
 

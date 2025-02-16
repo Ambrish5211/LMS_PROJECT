27209fe9c768eb78a63155f103af20652ea0b06e
Ambrish <ambrish19.5211@gmail.com>
2025-02-16 22:47:32 +0530

    fix 15

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-project-rhm9.onrender.com/" || "http://localhost:8000/api/v1"
+const BASE_URL = "http://localhost:8000/api/v1"
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
